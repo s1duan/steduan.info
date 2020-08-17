c1bfe3e2f855b4eb18250fa86c087e9445866273
stephen66778899@gmail.com <stephenduan@Stephens-MacBook-Pro.local>
2020-08-17 10:44:57 -0700

    test

---
+++ steduan.info/public_html/cgi-bin/php-general-echo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Hello PHP World!</title>
+</head>
+<body>
+    <h1>General Request Echo</h1>
+    <?php
+      $query = $_SERVER['QUERY_STRING'];
+      $protocol = $_ENV['SERVER_PROTOCOL'];
+      $method = $_ENV['REQUEST_METHOD'];
+      echo "<b>Request Method: </b>" . $method . "<br>";
+      echo "<b>Protocol: </b>" . $protocol . "<br>";
+      echo "<b>Query string:" . $query;
+      echo "<b>Message body: </b><br>";
+      foreach(array_keys($_POST) as $key) {
+          echo "<li><b>$key:</b>" . $_POST[$key] . "</li>";
+      }
+    ?>
+    
+</body>
+</html>
