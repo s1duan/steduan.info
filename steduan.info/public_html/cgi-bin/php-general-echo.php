a0c64d82e029ed5cebea71a3264cd136d2b5f38d
stephen66778899@gmail.com <stephenduan@Stephens-MacBook-Pro.local>
2020-08-17 10:47:48 -0700

    test

--- steduan.info/public_html/cgi-bin/php-general-echo.php
+++ steduan.info/public_html/cgi-bin/php-general-echo.php
@@ -9,11 +9,11 @@
     <h1>General Request Echo</h1>
     <?php
       $query = $_SERVER['QUERY_STRING'];
-      $protocol = $_ENV['SERVER_PROTOCOL'];
-      $method = $_ENV['REQUEST_METHOD'];
+      $protocol = $_SERVER['SERVER_PROTOCOL'];
+      $method = $_SERVER['REQUEST_METHOD'];
       echo "<b>Request Method: </b>" . $method . "<br>";
       echo "<b>Protocol: </b>" . $protocol . "<br>";
-      echo "<b>Query string:" . $query;
+      echo "<b>Query string:</b>" . $query . "<br>";
       echo "<b>Message body: </b><br>";
       foreach(array_keys($_POST) as $key) {
           echo "<li><b>$key:</b>" . $_POST[$key] . "</li>";
