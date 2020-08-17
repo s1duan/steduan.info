d0887ff966b7e328a86cb630de0822415c2d410f
stephen66778899@gmail.com <stephenduan@Stephens-MacBook-Pro.local>
2020-08-16 17:57:19 -0700

    php

---
+++ steduan.info/public_html/cgi-bin/php-hello-html-world.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Hello PHP World!</title>
+</head>
+<body>
+    <h1>Sicong Duan was here! </h1>
+    <?php
+      echo "Hello World"
+     ?>
+    
+</body>
+</html>
