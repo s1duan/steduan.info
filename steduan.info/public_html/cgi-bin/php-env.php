928dedb4f77c24145970431ecaa47efc18d6f624
stephen66778899@gmail.com <stephenduan@Stephens-MacBook-Pro.local>
2020-08-16 18:51:35 -0700

    test

---
+++ steduan.info/public_html/cgi-bin/php-env.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Hello PHP World!</title>
+</head>
+<body>
+    <h1>Environment Variable </h1>
+    <?php
+      foreach($_ENV as &$value) {
+        echo $value . "<br>";
+      }
+     ?>
+    
+</body>
