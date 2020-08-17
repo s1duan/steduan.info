f6a79ae10e3be9c6aaa044f9f45377d68e89985e
stephen66778899@gmail.com <stephenduan@Stephens-MacBook-Pro.local>
2020-08-17 10:13:10 -0700

    test

--- steduan.info/public_html/cgi-bin/php-hello-json-world.php
+++ steduan.info/public_html/cgi-bin/php-hello-json-world.php
@@ -4,10 +4,17 @@
 <body>
     <?php
       $ipaddress = getenv("REMOTE_ADDR");
+      $date = date("Y/m/d h:i:sa");
       date_default_timezone_set("America/Los_Angeles");
-      echo "{\"message\":\"Hello World from PHP!\",";
-      echo "\"date\":\"" . date("Y/m/d h:i:sa") . "\",";
-      echo "\"IPaddress\":\"" . $ipaddress . "\"}";
+      $myObject->message = "Hello world from PHP!";
+      $myObject->date = "$date";
+      $myObject->IPaddress = "$ipaddress";
+
+      $myJSON = json_encode($myObject);
+      echo $myJSON;
+      //echo "{\"message\":\"Hello World from PHP!\",";
+      //echo "\"date\":\"" . date("Y/m/d h:i:sa") . "\",";
+      //echo "\"IPaddress\":\"" . $ipaddress . "\"}";
      ?>
     
 </body>
