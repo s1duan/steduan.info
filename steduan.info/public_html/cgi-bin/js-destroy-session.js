82c4522b475ce34ff9c5dcbf233a6bfb3a5c9ff4
stephen66778899@gmail.com <stephenduan@Stephens-MacBook-Pro.local>
2020-08-18 21:24:37 -0700

    js-fix

--- steduan.info/public_html/cgi-bin/js-destroy-session.js
+++ steduan.info/public_html/cgi-bin/js-destroy-session.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 console.log('Cache-Control: no-cache');
-console.log(`Set-Cookie:username=`)
+console.log(`Set-Cookie:jsusername=`)
 console.log("Content-type: text/html\n\n");
 console.log(`<html><head><title>JS Session Destroyed</title></head><body><h1>Session Destroyed</h1><a href="/cgi-bin/js-cgiform.js">Back to the JS CGI Form</a><br /><a href="/cgi-bin/js-sessions-1.js">Back to Page 1</a><br /><a href="/cgi-bin/js-sessions-2.js">Back to Page 2</a></body></html>`)
